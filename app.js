69b627f84d58f99d7a1994290365555b2da8ee45
marshmello <pesbg123@gmail.com>
2023-07-17 22:26:03 +0900

    fix: 딜리버리 리퀘스트 제거 feat: test로 메뉴 CR 작성 + 주문 C

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
 
+const menuRouter = require('./routes/menus.router');
+
 app.use(express.json()); // json 파싱
 app.use(cookieParser()); // 쿠키 파싱
 
 app.use(express.static('public'));
 
-// app.use('/api', []);
+app.use('/api', [menuRouter]);
 
 app.listen(PORT, () => {
   console.log(`${PORT}번 포트로 서버가 열렸습니다.`);
