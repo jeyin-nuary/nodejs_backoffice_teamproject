58f4e31b4b2db634d78facce613241d9afec9f72
jeyin-nuary <122019196+jeyin-nuary@users.noreply.github.com>
2023-07-18 12:19:48 +0900

    test: first commit

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,4 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const { Users } = require('../models');
+
+// 회원가입 api
+router.post('/signup', async (req, res) => {
+  try {
+    const { email, password, confirmPassword, nickname, points, userAddress } = req.body;
+    // 중복되는 이메일 찾기
+    const overlappedEmail = await Users.findOne({ where: { email } });
+
+    // 중복되는 닉네임 찾기
+    const overlappedNickname = await Users.findOne({where: {nickname}});
+    
+    if (overlappedNickname) {
+      return res.status()
+    }
+  } catch (error) {}
+});
 
 module.exports = router;
