89f201d31df2fd2640d68467840e3943cc569e7b
marshmello <pesbg123@gmail.com>
2023-07-18 19:35:49 +0900

    test

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+const menuRouter = require('./routes/menus.router');
+
+app.use(express.json()); // json 파싱
+app.use(cookieParser()); // 쿠키 파싱
+
+app.use(express.static('public'));
+
+app.use('/api', [menuRouter]);
+
+app.listen(PORT, () => {
+  console.log(`${PORT}번 포트로 서버가 열렸습니다.`);
+});
