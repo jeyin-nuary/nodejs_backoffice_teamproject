c8017c23ca7189be08a0cfc5ef42c55a30aba5f2
marshmello <pesbg123@gmail.com>
2023-07-20 02:33:49 +0900

    feat: 가게 상제 조회 api 추가

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
 
-const menuRouter = require('./routes/menus.router');
+const storeRouter = require('./routes/stores.router');
 
 app.use(express.json()); // json 파싱
 app.use(cookieParser()); // 쿠키 파싱
 
 app.use(express.static('public'));
 
-app.use('/api', [menuRouter]);
+app.use('/api', [storeRouter]);
 
 app.listen(PORT, () => {
   console.log(`${PORT}번 포트로 서버가 열렸습니다.`);
--- routes/stores.router.js
+++ routes/stores.router.js
@@ -1,4 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const { Stores, Menus, Reviews } = require('../models');
+// Stores에서 storeName, storeAddress, storeUrl, storeRating
+// Menus에서 menuName, menuUrl
+// Reviews에서 reviewContent, reviewUrl
+
+// 가게 상세 페이지 조회 api
+// 가게 이름, 가게 주소, 가게 사진, 메뉴 이름, 메뉴사진, 리뷰 조회
+router.get('/stores/:storeId', async (req, res) => {
+  const { storeId } = req.params;
+  const getStore = await Stores.findOne({
+    where: { storeId },
+    attributes: ['storeName', 'storeAddress', 'storeUrl', 'storeRating'],
+    include: [
+      {
+        models: Menus,
+        attributes: ['menuName', 'menuUrl'],
+      },
+      {
+        models: Reviews,
+        attributes: ['reviewContent', 'reviewUrl'],
+      },
+    ],
+  });
+  res.status(200).json({ data: getStore });
+});
+
+// 스토어 등록 test
+router.post('/stores', async (req, res) => {
+  const store = await Stores.create({
+    userId: 1,
+    storeName: 'BHC강남점',
+    storeAddress: '강남',
+    storeUrl: 'img.jpg',
+    storeRating: 10,
+  });
+  res.json({ data: store });
+});
 
 module.exports = router;
