0379b8e4c09b926263f1d166ed6ab19f7e65e2fd
shimjaedoo <woen1637@naver.com>
2023-07-20 09:19:17 +0900

    가게목록 페이지 / 가게 등록페이지 구현완료

--- public/storeList.js
+++ public/storeList.js
@@ -1,16 +1,20 @@
-const storeLists = document.getElementById('storeLists');
+// 브라우저에 동적으로 표시하기 위해 파일을 만들었습니다. 저희는 ejs를 쓰지 않기에 render가 아닌 fetch로 연결했습니다.
 
+// storeLists 변수 선언 한뒤 storeList.html 파일에 id값이 storeLists 값을 찾고 storeLists 변수에 담음
+const storeLists = document.getElementById('storeLists');
+// 밑에 fetchStoreLists 함수에 있는 데이터를 가져올려고 호출
 fetchStoreLists();
 
-// 가게 목록 데이터를 가져오는 함수
+// API 에서 가게 목록 데이터를 가져오는 함수
 async function fetchStoreLists() {
   try {
-    const response = await fetch('/api/storelists');
-    const { data } = await response.json();
-
+    const response = await fetch('/api/storelists'); // 이 부분 fetch를 통해 API를 가져옴
+    const { data } = await response.json(); // 가져온 데이터를 json형식으로 반환
+    // store(파라미터)로 각 가게들 정보를 담아서 foreach를 통해 차례대로 돌아감
     data.forEach(store => {
       if (store.storeUrl == 'NULL') {
-        storeLists.innerHTML += `
+        // 아래의 += 를 통해 계속 더해짐
+        storeLists.innerHTML += ` 
       <div class="card mb-5">
   <div class="card-header">
    평점: ${'★'.repeat(store.storeRating)}
--- routes/stores.router.js
+++ routes/stores.router.js
@@ -5,6 +5,7 @@ const { Sequelize } = require('sequelize');
 // 가게 목록 페이지 (API 명세없음)
 router.get('/storelists', async (req, res) => {
   try {
+    // findAll을 통해 DB에서 attributes로 가져올것들을 정함
     const storeList = await Stores.findAll({
       attributes: [
         'storeId',
@@ -14,11 +15,14 @@ router.get('/storelists', async (req, res) => {
         'storeRating',
         'createdAt',
         'updatedAt',
+        // sequelize 문법으로 COUNT를 이용해 starRating 이라는 임시 이름으로 갯수를 계산하여 추가함
         [Sequelize.fn('COUNT', Sequelize.col('Stores.storeRating')), 'starRating'],
       ],
+      // order : createdAt 별로 내림차순, group: 'Stores.storeId별로 그룹화
       order: [['createdAt', 'DESC']],
       group: ['Stores.storeId'],
     });
+    // 유효성 검사
     if (!storeList)
       return res.status(400).json({ errorMessage: '가게 목록을 불러오지 못했습니다.' });
     return res.status(200).json({ data: storeList });
@@ -28,7 +32,31 @@ router.get('/storelists', async (req, res) => {
 });
 
 // (관리자) 가게 등록
-// router.post('/stores',async (req,res)=>{
-
-// })
+router.post('/stores', async (req, res) => {
+  try {
+    const { storeName, storeAddress, storeUrl } = req.body;
+    // 중복된 이름의 가게가 있는지 확인하기 위해서
+    const existingStore = await Stores.findOne({ where: { storeName } });
+    // 유효성 검사
+    if (existingStore) {
+      return res.status(409).json({
+        errorMessage: '이미 등록된 가게입니다!!!',
+      });
+    }
+    if (!storeName) return res.status(403).json({ errorMessage: '가게 이름을 입력해주세요.' });
+    if (!storeAddress) return res.status(403).json({ errorMessage: '가게 주소를 입력해주세요.' });
+    // 가게등록을 생성하고 DB에 저장하는 과정
+    const storeRegister = await Stores.create({
+      storeName,
+      storeAddress,
+      storeUrl,
+    });
+    // 유효성 검사
+    if (!storeRegister)
+      return res.status(400).json({ errorMessage: '가게 등록에 실패하였습니다.' });
+    return res.status(200).json({ data: storeRegister });
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 module.exports = router;
