abb62bc7c7bda55f5ddca4825a421fa3bc826453
k0723 <kimbum7824@gmail.com>
2023-07-20 23:08:50 +0900

    Revert "Revert "메뉴 검색""
    This reverts commit d671099ee3be0b1ac95f3822b00cd49c49efede1.

--- routes/menus.router.js
+++ routes/menus.router.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const { Menus } = require('../models');
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 // 테스트
 router.post('/menus', async (req, res) => {
@@ -10,7 +12,7 @@ router.post('/menus', async (req, res) => {
     storeId,
     menuName,
     menuUrl,
-    menuDescription,
+    menuInfo : menuDescription,
     menuPrice,
   });
   res.json({ message: 'create' });
@@ -21,5 +23,25 @@ router.get('/menus', async (req, res) => {
   const menus = await Menus.findAll({});
   res.json({ data: menus });
 });
+
+router.get('/menus/:search', async(req,res)=> {
+  const {search} = req.params;
+
+  if(!search){
+    return res.status(404).json({ "message": "입력을 다시 해주세요" });
+  }
+  
+  const searchmenu = await Menus.findAll({where: {
+          menuName: {
+            [Op.like]: `%${search}%`
+          }
+        }
+      }
+    );
+  if(Object.keys(searchmenu).length === 0){
+    return res.status(404).json({ "message": "해당 검색어를 포함한 메뉴가 존재하지 않습니다." });
+  }
+  res.json({data: searchmenu})
+})
 
 module.exports = router;
